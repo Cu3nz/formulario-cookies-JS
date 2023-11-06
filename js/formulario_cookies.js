ceac9927fe68115b2d54c58cf5dab9c1b4f12f7b
Cu3nz <sergiogallegit@gmail.com>
2023-11-06 21:53:25 +0100

    Paginas de validacion y de error añadidas, validacion fecha implementada, pagina validacion muestra lo que almacena las cookies

--- js/formulario_cookies.js
+++ js/formulario_cookies.js
@@ -1,3 +1,6 @@
+function pad(n) { 
+  return n < 10 ? '0' + n : n; //? Si n es menor que 10, pongo un 0 + el numero (2 es menor que 10 por lo tanto --> 02), si no devuelve el numero 10 , 11 12 ....
+}
 
 //? Funcion para que se guarde la cookie una hora
 function setCookie(cname, cvalue) {
@@ -35,20 +38,27 @@ function setCookie(cname, cvalue) {
     //todo Recogemos lo que el usuario escribe por cada input
     let input_nombre = document.getElementById("nombre").value.trim();
 
+    //! --------------------------------------- VALIDACION DE NOMBRE -------------------------------------
+    
     let longitudNombre = 4
-
+    
     if (input_nombre.length < longitudNombre){ //* Si la longitud de caracteres que introduce el usuario por el input es menor a la longitud definida, mostramos error.
       problemaValidacion = true;
       console.log("%cError: El nombre debe tener al menos " + longitudNombre + " caracteres. Por favor, verifica e intenta de nuevo.", "color: orange; font-size: 16px; ");
     }
+    //! --------------------------------------- FIN VALIDACION DE NOMBRE -------------------------------------
 
     let input_apellidos = document.getElementById("apellidos").value.trim();
     /* let input_dni = document.getElementById("dni").value; */ //! lo recogemos en la validacion del dni
     let input_telefono = document.getElementById("telefono").value.trim();
 
+    //! -----------------------------------------  VALIDACION DE TELÉFONO  ----------------------------------
+    
     if (input_telefono.length !== 9){
       console.log("%cError: El telefono debe contener " + 9 + " numeros. Por favor, verifica e intenta de nuevo.", "color: orange; font-size: 16px; ");
     }
+
+    //! -----------------------------------------  FIN VALIDACION DE TELÉFONO  ----------------------------------
 
     /* let input_email = document.getElementById("email").value; */ //! lo recogemos en la validacion del email
 
@@ -58,9 +68,6 @@ function setCookie(cname, cvalue) {
     //* Validacion de email
     //? Si hay algun error, muestro un mensaje por pantalla de que hay un error y que no se han almacenado las cookies 
     //* Si no hay ningun error, muestro un mensaje por pantalla de que se han guardado y las guardo con setcookie
-    
-   
-
     
 
     //!  Validacion de dni 
@@ -101,72 +108,214 @@ function setCookie(cname, cvalue) {
     }
 
 
-    //! Validacion de email
-
+    //! ---------------------------------------- VALIDACION DE EMAIL -----------------------------
+    
     var input_email = document.getElementById("email").value.trim(); //* el value lo pongo para obtener el valor actual del input
-
+    
     //* Primero tengo que sacar en que posicion esta el @
+    
+    var arroba = input_email.indexOf("@");
+    
+    //*  y sacar la posicion de donde esta el ultimo punto.
+    var ultimoPunto = input_email.lastIndexOf(".");
+    
+    if (arroba === -1 && ultimoPunto === -1) { //! si no existe ni el . , ni el @ te muestra el siguiente mensaje de error
+      console.log("%cError: el correo no tiene ni la @ , ni el .", "color: red; font-size: 16px;");
+      problemaValidacion = true //? Existe un problema de validacion
+    } else if (arroba === -1) { //!si no esta el @ mostramos el error
+      console.log("%cError: el correo no tiene un @", "color: red; font-size: 16px;");
+      problemaValidacion = true; //? Existe un problema de validacion
+    } else if (ultimoPunto === -1) { //! si no existe el punto mostramos el error.
+      console.log("%cError: el correo no tiene un .", "color: red; font-size: 16px;");
+      problemaValidacion = true; //? Existe un problema de validacion
+    } else if (arroba === input_email.length - 1) { //! si el arroba esta en la ultima posicion, es porque despues del arroba no hay ninguna cadena
+      console.log(
+        "%c Error: el @ no puede estar en la ultima posción",
+        "color:red; font-size: 16px;"
+        );
+        problemaValidacion = true; //? Existe un problema de validacion
+      } else if (arroba === 0) {//! si el arroba esta en la posicion 0 es porque delante del arroba no hay una cadena
+        console.log(
+          "%cError: el @ no puede estar en la primera posición",
+          "color: red; font-size: 16px;"
+          );
+          problemaValidacion = true; //? Existe un problema de validacion
+        } else if (ultimoPunto <= arroba + 1) { //! Si entre el punto y el arroba no hay ningun texto muestro el siguiente mensaje 
+          console.log("%cError:  No hay texto entre el @ y el punto", "color: red; font-size: 16px; ");
+          problemaValidacion = true; //? Existe un problema de validacion
+        } else { //* Si no es porque el correo esta bien 
+          console.log("%cEl gmail es valido", "color: green; font-size:16px;");
+        }
 
-  var arroba = input_email.indexOf("@");
+        //! ---------------------------------------- FIN VALIDACION DE EMAIL -----------------------------
+        
+        
+        //!  ------------------------------------- VALIDACION PARA QUE LOS CAMPOS NO ESTEN VACIOS ----------------------------------------
+        if (input_nombre == "" || input_apellidos == "" || input_dni == "" || input_telefono == "" || input_email == ""){
+          problemaValidacion = true;
+          console.log("%cError: No puede haber ningun campo vacio", "color: orange; font-size:17px;");
+        }
 
-  //*  y sacar la posicion de donde esta el ultimo punto.
-  var ultimoPunto = input_email.lastIndexOf(".");
-
-  if (arroba === -1 && ultimoPunto === -1) { //! si no existe ni el . , ni el @ te muestra el siguiente mensaje de error
-    console.log("%cError: el correo no tiene ni la @ , ni el .", "color: red; font-size: 16px;");
-    problemaValidacion = true //? Existe un problema de validacion
-  } else if (arroba === -1) { //!si no esta el @ mostramos el error
-    console.log("%cError: el correo no tiene un @", "color: red; font-size: 16px;");
-    problemaValidacion = true; //? Existe un problema de validacion
-  } else if (ultimoPunto === -1) { //! si no existe el punto mostramos el error.
-    console.log("%cError: el correo no tiene un .", "color: red; font-size: 16px;");
-    problemaValidacion = true; //? Existe un problema de validacion
-  } else if (arroba === input_email.length - 1) { //! si el arroba esta en la ultima posicion, es porque despues del arroba no hay ninguna cadena
-    console.log(
-      "%c Error: el @ no puede estar en la ultima posción",
-      "color:red; font-size: 16px;"
-    );
-    problemaValidacion = true; //? Existe un problema de validacion
-  } else if (arroba === 0) {//! si el arroba esta en la posicion 0 es porque delante del arroba no hay una cadena
-    console.log(
-      "%cError: el @ no puede estar en la primera posición",
-      "color: red; font-size: 16px;"
-    );
-    problemaValidacion = true; //? Existe un problema de validacion
-  } else if (ultimoPunto <= arroba + 1) { //! Si entre el punto y el arroba no hay ningun texto muestro el siguiente mensaje 
-    console.log("%cError:  No hay texto entre el @ y el punto", "color: red; font-size: 16px; ");
-    problemaValidacion = true; //? Existe un problema de validacion
-  } else { //* Si no es porque el correo esta bien 
-    console.log("%cEl gmail es valido", "color: green; font-size:16px;");
-  }
+        //!  ------------------------------------- FIN VALIDACION PARA QUE LOS CAMPOS NO ESTEN VACIOS ----------------------------------------
 
 
-  //!  El nombre, apellidos dni telefono y email no pueden estar vacios 
-  if (input_nombre == "" || input_apellidos == "" || input_dni == "" || input_telefono == "" || input_email == ""){
-    problemaValidacion = true;
-    console.log("%cError: No puede haber ningun campo vacio", "color: orange; font-size:17px;");
-  }
+        //! ------------------------------------- VALIDACION PARA LA FECHA -------------------------------------
+
+        let fechaInput = document.getElementById("fechaNacimiento").value.trim(); //* Recogemos la fecha que el usuario ha introducido por teclado
+
+        const partesFecha = fechaInput.split("/");  //* Dividimos la fecha en partes usando el separador /, devuelve un array con 3 indices [0] => dia, [1] => mes , [2] => año
+
+        //console.log(partesFecha);
+
+        if (partesFecha.length === 3){ //* Si la fecha tiene 3 partes 
+
+          const dia = parseInt(partesFecha[0] , 10); //? Tomamos el primer indice del array en este caso el dia y lo pasamos a entero, el 10 indica que la conserviosn se debe hacer en base decimal, para que ponga 8 en decimal
+          const mes = parseInt(partesFecha[1] , 10) //? tomamos el segundo indice en este caso el mes y lo pasamos a enteero, el 10 indica que la conversion se debe hacer en base decimal.
+          const anio = parseInt(partesFecha[2] , 10) //? Tomamos el tercer indice en este caso el año y lo pasamos a entero, el 10 indica que la conversion se debe hacer en base decimal.
+
+          console.log("Día:", dia, "Mes:", mes, "Año:", anio);
+
+
+          //todo Empezamos con las validaciones
+
+          if (dia < 1 || dia > 31){ //* Si el dia se pasa del rango de 1 y 31, tenemos un error de validacion 
+            problemaValidacion = true;
+            console.log("Error de validación: El día está fuera de rango.");
+
+          }
+
+          if (mes < 1 || mes > 12){ //* Si el mes se pasa del rango de 1-12, tenemos un error de validacion
+            problemaValidacion = true;
+            console.log("Error de validación: El mes está fuera de rango.");
+          }
+
+          if (anio.toString().length != 4){ //* Pasamos el año a string si el numero de caracteres de anio es distinto de 4, tenemos un error de validacion (porque el año tiene 4 numeros).
+            problemaValidacion = true;
+            console.log("Error de validación: El año no tiene cuatro dígitos.");
+
+          }
+
+          if ((mes === 4 || mes === 6 || mes === 9 || mes === 11 ) && dia === 31){ //* Verifica si el mes es abril, junio, septiembre o noviembre, que son los meses que tienen 30 días. Si alguno de estos meses tiene asignado el día 31, marca un error de validación,ya que estos meses solo deben tener hasta 30 días.
+            problemaValidacion = true;
+            console.log("Error de validación: El mes no tiene 31 días.");
+
+          }
+         // Verifica si el mes es febrero (mes 2).
+        if (mes === 2){
+        // Calcula si el año es bisiesto. Un año es bisiesto si es divisible por 4,
+        // excepto aquellos años que son divisibles por 100, a menos que también sean divisibles por 400.
+        const esBisiesto = (anio % 4 === 0 && (anio % 100 !== 0 || anio % 400 === 0));
+
+        // Si el día es mayor a 29, o si es 29 y el año no es bisiesto (febrero solo tiene 29 días en un año bisiesto),
+        // entonces hay un problema de validación.
+        if (dia > 29 || (dia === 29 && !esBisiesto)) {
+        problemaValidacion = true;
+        console.log("Error de validación: Febrero no tiene más de 29 días o no es año bisiesto y se está intentando poner el día 29.");
+        }
+      } else {
+        console.log("La fecha es correcta");
+      }
+
+        } else { //* Si la fecha no tiene 3 partes 
+
+          console.log("Error, la fecha no tiene 3 partes");
+
+        }
+
+
+
+
+        //! ------------------------------------- FIN VALIDACION PARA LA FECHA -------------------------------------
 
     
 
   //? Si hay algun problema de validacion, muestro un mensaje por pantalla de que hay un error y que no se han almacenado las cookies 
 
   if (problemaValidacion){
-    console.log('%c ***** Error no se han podido almacenar las cookies, verifica tus datos con los errores mostrados por consola e intentalo de nuevo  ', 'color: red; font-size: 16px;');
+    const url = "problemas_validacion.html";
+
+    // Dimensiones de la ventana que quieres abrir
+    var ancho = 550;
+    var alto = 800;
+
+    // Calcula la posición x e y para centrar la ventana
+    var posicionX = (window.screen.width / 2) - (ancho / 2);
+    var posicionY = (window.screen.height / 2) - (alto / 2);
+
+    // Asegúrate de que las posiciones no sean negativas
+    posicionX = posicionX < 0 ? 0 : posicionX;
+    posicionY = posicionY < 0 ? 0 : posicionY;
+
+    // Abre la nueva ventana centrada
+    var nuevaVentana = window.open(url, "ventana1", "width=" + ancho + ",height=" + alto + ",left=" + posicionX + ",top=" + posicionY + ",scrollbars=YES,toolbar=NO,status=NO,resizable=YES,menubar=NO,location=NO,directories=NO");
+
+    // Enfoca la nueva ventana en caso de que el navegador lo permita
+    if (window.focus) {
+        nuevaVentana.focus();
+    }
+
+    console.log('%c ***** Error no se han podido almacenar las cookies, verifica tus datos con los errores mostrados por consola e intenta de nuevo  ', 'color: red; font-size: 16px;');
 
   } else { //? Si no hay ningun error, muestro un mensaje por pantalla de que se han guardado y las guardo con setcookie
 
+    const url = "validacion_correcta.html";
+
+    // Dimensiones de la ventana que quieres abrir
+    var ancho = 550;
+    var alto = 800;
+
+    // Calcula la posición x e y para centrar la ventana
+    var posicionX = (window.screen.width / 2) - (ancho / 2);
+    var posicionY = (window.screen.height / 2) - (alto / 2);
+
+    // Asegúrate de que las posiciones no sean negativas
+    posicionX = posicionX < 0 ? 0 : posicionX;
+    posicionY = posicionY < 0 ? 0 : posicionY;
+
+    // Abre la nueva ventana centrada
+    var nuevaVentana = window.open(url, "ventana1", "width=" + ancho + ",height=" + alto + ",left=" + posicionX + ",top=" + posicionY + ",scrollbars=YES,toolbar=NO,status=NO,resizable=YES,menubar=NO,location=NO,directories=NO");
+
+    // Enfoca la nueva ventana en caso de que el navegador lo permita
+    if (window.focus) {
+        nuevaVentana.focus();
+    }
+
     console.log('%cSe han guardado las cookies de forma exitosa', 'color: green; font-size: 17px;');
-    setCookie("nombre" , input_nombre , 1); // Almacena el nombre que ha introducido el usuario por el input durante 1 hora
-    setCookie("apellidos" , input_apellidos , 1); // Almacena el Apellido que ha introducido el usuario por el input durante 1 hora
-    setCookie("dni" , input_dni , 1); // Almacena el dni que ha introducido el usuario por el input durante 1 hora
-    setCookie("telefono" , input_telefono , 1); // Almacena el telefono que ha introducido el usuario por el input durante 1 hora
-    setCookie("email" , input_email , 1); // Almacena el email que ha introducido el usuario por el input durante 1 hora
+    
+    //todo Almacenamos en las cookies lo que introuce el usuario en los inputs 
+      setCookie("nombre" , input_nombre , 1); // Almacena el nombre que ha introducido el usuario por el input durante 1 hora
+      setCookie("apellidos" , input_apellidos , 1); // Almacena el Apellido que ha introducido el usuario por el input durante 1 hora
+      setCookie("dni" , input_dni , 1); // Almacena el dni que ha introducido el usuario por el input durante 1 hora
+      setCookie("telefono" , input_telefono , 1); // Almacena el telefono que ha introducido el usuario por el input durante 1 hora
+      setCookie("email" , input_email , 1); // Almacena el email que ha introducido el usuario por el input durante 1 hora
+      //! falta la fecha tengo que hacer validacion.
+  }
+  }
+
+
+   //! ------------------------------ FUNCION QUE DEVUELVE LO QUE ALMACENAS LAS COOKIES EN OTRA PAGINA (VALIDACION_CORRECTA.HTML) -----------
+  function mostrarDatosValidacionCorrecta(){
+    //todo Devolvemos lo que almacenan las cookies 
+
+    return{
+      nombre:getCookie("nombre"),
+      apellidos:getCookie("apellidos"),
+      dni:getCookie("dni"),
+      telefono:getCookie("telefono"),
+      email:getCookie("email")
+    };
   }
 
 
 
-  }
+
+
+
+
+
+
+
+
 
   //todo Funcion para saber si existe o no una cookie
 
